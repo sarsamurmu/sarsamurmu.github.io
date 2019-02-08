62bdf06d63189859b77498420451e9cf1e69e0a8
Sarsa Murmu <sarsamurmu05@gmail.com>
2019-02-08 21:21:42 +0530

    Mistake

--- scripts/sidemenu.js
+++ scripts/sidemenu.js
@@ -23,18 +23,21 @@ function endDim() {
   document.getElementById("dim-container").style.width = "0%";
   document.getElementById("dim-container").style.transitionDelay = "0.55s";
   dimmer.classList.remove("menu--visible");
+toggleSideMenu();
 }
 
 function OnTransitionEnd() {
 	sideMenu.classList.remove("menu--animatable");
 }
 
-var sideMenu = document.querySelector(".sidemenu");
+var sideMenu = document.querySelector(".sidemenu");
+var oppMenu = document.querySelector(".menu-icon");
 var dimmer = document.querySelector(".dimmer");
 var body = document.querySelector(".body");
 var projecttext = document.querySelector(".projecttext");
 sideMenu.addEventListener("transitionend", OnTransitionEnd, false);
-projecttext.addEventListener("click", toggleSideMenu, false);
+projecttext.addEventListener("click", toggleSideMenu, false);
+oppMenu.addEventListener("click", toggleSideMenu, false);
 dimmer.addEventListener("click", toggleSideMenu, false);
 function closeNav(){ endDim(); toggleSideMenu(); }
 function openNav(){ startDim(); toggleSideMenu(); }
