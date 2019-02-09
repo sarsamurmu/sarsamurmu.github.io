e82b7cbbd67f9c7053224386a2dfd67ecc364293
Sarsa Murmu <sarsamurmu05@gmail.com>
2019-02-09 13:25:35 +0530

    Min

--- scripts/sidemenu.js
+++ scripts/sidemenu.js
@@ -1,30 +1,21 @@
-function toggleSideMenu() {
+function openSideMenu() {
 	sideMenu.classList.add("menu--animatable");	
-	if(!sideMenu.classList.contains("menu--visible")) {		
-		sideMenu.classList.add("menu--visible");
-	} else {
-		sideMenu.classList.remove('menu--visible');		
-	}
-	<!-- Disable Scrolling Body when Sidemenu is opened -->
-	if(!body.classList.contains("lock")) {		
-		body.classList.add("lock");
-	} else {
-		body.classList.remove('lock');		
-	}
-}
-
-function startDim() {
-  document.getElementById("dim-container").style.width = "100%";
+	sideMenu.classList.add("menu--visible");
+  body.classList.add("lock");
+ document.getElementById("dim-container").style.width = "100%";
   document.getElementById("dim-container").style.transition = "0s";
   dimmer.classList.add("menu--visible");
 }
 
-function endDim() {
-  document.getElementById("dim-container").style.width = "0%";
+function closeSideMenu() {
+	sideMenu.classList.add("menu--animatable");	
+	sideMenu.classList.remove("menu--visible");
+  body.classList.remove("lock");
+ document.getElementById("dim-container").style.width = "0%";
   document.getElementById("dim-container").style.transitionDelay = "0.55s";
   dimmer.classList.remove("menu--visible");
 toggleSideMenu();
-}
+}
 
 function OnTransitionEnd() {
 	sideMenu.classList.remove("menu--animatable");
@@ -36,8 +27,6 @@ var dimmer = document.querySelector(".dimmer");
 var body = document.querySelector(".body");
 var projecttext = document.querySelector(".projecttext");
 sideMenu.addEventListener("transitionend", OnTransitionEnd, false);
-projecttext.addEventListener("click", toggleSideMenu, false);
-oppMenu.addEventListener("click", toggleSideMenu, false);
-dimmer.addEventListener("click", toggleSideMenu, false);
-function closeNav(){ endDim(); toggleSideMenu(); }
-function openNav(){ startDim(); toggleSideMenu(); }
+projecttext.addEventListener("click", closeSideMenu, false);
+oppMenu.addEventListener("click", openSideMenu, false);
+dimmer.addEventListener("click", closeSideMenu, false);
